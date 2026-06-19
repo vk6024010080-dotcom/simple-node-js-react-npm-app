b17c8210d938157b5e4294160b5f8981122dd1c5
Kuy Visal <kuyvisal413@gmail.com>
2026-06-19 21:38:57 +0700

    Add initial Jenkinsfile

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -22,4 +22,4 @@ echo $! > .pidfile
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
+echo 'Visit http://localhost:3001 to see your Node.js/React application in action.'
